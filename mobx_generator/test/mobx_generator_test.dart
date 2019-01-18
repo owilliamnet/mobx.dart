ed3bfc3c69075ed72d4f3d3112c3e9cfa280e715
copydog <775150@qq.com>
2019-01-18 20:33:57 +0800

    [generator]refactor: clean up test

--- mobx_generator/test/mobx_generator_test.dart
+++ mobx_generator/test/mobx_generator_test.dart
@@ -1,16 +1,4 @@
 import 'package:mobx_generator/mobx_generator.dart';
 import 'package:test/test.dart';
 
-void main() {
-  group('A group of tests', () {
-    Awesome awesome;
-
-    setUp(() {
-      awesome = new Awesome();
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
-}
+void main() {}
